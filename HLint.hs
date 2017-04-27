1c4371d026a7743367ff478ee2e1b72adeaa2158
Ryan Scott <ryan.gl.scott@gmail.com>
2017-04-27 11:04:11 -0400

    Shut up, hlint

---
+++ HLint.hs
@@ -0,0 +1,8 @@
+import "hint" HLint.HLint
+
+ignore "Reduce duplication"
+ignore "Redundant lambda"
+ignore "Use >=>"
+ignore "Use const"
+ignore "Use module export list"
+ignore "Use newtype instead of data"
