392d3a1f9e15a8c6198ff396e88c128a47b6177d
Siddharth Bhat <siddu.druid@gmail.com>
2018-10-05 22:23:59 +0530

    [WIP] writing fg

--- tests/unit.hs
+++ tests/unit.hs
@@ -1,9 +1,31 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE RoleAnnotations #-}
+{-# LANGUAGE TemplateHaskell #-}
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
 import Data.List
 import Data.Ord
+
+import Data.Struct
+import Data.Struct.TH
+
+
+makeStruct [d|
+  data LinkCut a s = LinkCut
+    { path, parent, left, right :: !(LinkCut a s)
+    , value, summary :: a
+    }
+   |]
+
+makeStruct [d|
+  data TupleInts a s  = TupleInts
+    { tupleLeft, tupleRight :: a
+    } 
+    |]
+
+-- mkTupleInts a b = st (newTupleInts Nil Nil a b)
 
 main = defaultMain tests
 
@@ -18,17 +40,10 @@ qcProps = testGroup "(checked by QuickCheck)"
       \list -> sort (list :: [Int]) == sort (reverse list)
   , QC.testProperty "Fermat's little theorem" $
       \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , QC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
   ]
+
 
 unitTests = testGroup "Unit tests"
   [ testCase "List comparison (different length)" $
       [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
   ]
