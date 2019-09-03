2c1ec7a5e002dcb0438e8b6054221f986e2be8b9
Ryan Scott <ryan.gl.scott@gmail.com>
2019-09-03 15:51:48 -0400

    Migrate HLint.hs to .hlint.yaml

--- HLint.hs
+++
@@ -1,8 +0,0 @@
-import "hint" HLint.HLint
-
-ignore "Reduce duplication"
-ignore "Redundant lambda"
-ignore "Use >=>"
-ignore "Use const"
-ignore "Use module export list"
-ignore "Use newtype instead of data"
